cebf3404f0f7a51a25d3a6f24d71b96d81189165
Andrey Prihodko <aprihodko@gmail.com>
2017-10-27 19:00:30 +0300

    React Native 0.49 support

--- SearchBar.js
+++ SearchBar.js
@@ -1,73 +1,79 @@
-var NativeModules, PropTypes, RNSearchBar, React, ReactNative, SearchBar;
+import React from "react";
+import {NativeModules, requireNativeComponent} from "react-native";
+import PropTypes from 'prop-types';
 
-React = require('react');
 ReactNative = require('react-native');
-PropTypes = require('prop-types');
 
-RNSearchBar = ReactNative.requireNativeComponent('RNSearchBar', null);
-NativeModules = ReactNative.NativeModules;
+const RNSearchBar = requireNativeComponent('RNSearchBar', null);
 
-SearchBar = React.createClass({
-  propTypes: {
-    placeholder: PropTypes.string,
-    text: PropTypes.string,
-    barTintColor: PropTypes.string,
-    tintColor: PropTypes.string,
-    textColor: PropTypes.string,
-    textFieldBackgroundColor: PropTypes.string,
-    showsCancelButton: PropTypes.bool,
-    onChange: PropTypes.func,
-    onChangeText: PropTypes.func,
-    onFocus: PropTypes.func,
-    onBlur: PropTypes.func,
-    onSearchButtonPress: PropTypes.func,
-    onCancelButtonPress: PropTypes.func,
-    enablesReturnKeyAutomatically: PropTypes.bool,
-    hideBackground: PropTypes.bool,
-    barStyle: PropTypes.oneOf(['default', 'black']),
-    searchBarStyle: PropTypes.oneOf(['default', 'prominent', 'minimal']),
-    editable: PropTypes.bool
-  },
-  getDefaultProps: function() {
-    return {
-      barStyle: 'default',
-      searchBarStyle: 'default',
-      editable: true
-    };
-  },
-  _onChange: function(e) {
-    var base, base1;
+class SearchBar extends React.Component {
+  _onChange = (e) => {
+    let base, base1;
     if (typeof (base = this.props).onChange === "function") {
       base.onChange(e);
     }
     return typeof (base1 = this.props).onChangeText === "function" ? base1.onChangeText(e.nativeEvent.text) : void 0;
-  },
-  _onPress: function(e) {
-    var base, base1, button;
+  };
+
+  _onPress = (e) => {
+    let base, base1, button;
     button = e.nativeEvent.button;
     if (button === 'search') {
       return typeof (base = this.props).onSearchButtonPress === "function" ? base.onSearchButtonPress(e.nativeEvent.searchText) : void 0;
     } else if (button === 'cancel') {
       return typeof (base1 = this.props).onCancelButtonPress === "function" ? base1.onCancelButtonPress() : void 0;
     }
-  },
-  blur: function() {
+  };
+
+  blur() {
     return NativeModules.RNSearchBarManager.blur(ReactNative.findNodeHandle(this));
-  },
-  focus: function() {
-    return NativeModules.RNSearchBarManager.focus(ReactNative.findNodeHandle(this));
-  },
-  unFocus: function() {
-    return NativeModules.RNSearchBarManager.unFocus(ReactNative.findNodeHandle(this));
-  },
-  render: function() {
-    return <RNSearchBar
-      style={{height: NativeModules.RNSearchBarManager.ComponentHeight}}
-      onChange={this._onChange}
-      onPress={this._onPress}
-      {...this.props}
-    />;
   }
-});
+
+  focus() {
+    return NativeModules.RNSearchBarManager.focus(ReactNative.findNodeHandle(this));
+  }
+
+  unFocus() {
+    return NativeModules.RNSearchBarManager.unFocus(ReactNative.findNodeHandle(this));
+  }
+
+  render() {
+    return (
+      <RNSearchBar
+        style={NativeModules.RNSearchBarManager.ComponentHeight}
+        onChange={this._onChange}
+        onPress={this._onPress}
+        {...this.props}
+      />
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  placeholder: PropTypes.string,
+  text: PropTypes.string,
+  barTintColor: PropTypes.string,
+  tintColor: PropTypes.string,
+  textColor: PropTypes.string,
+  textFieldBackgroundColor: PropTypes.string,
+  showsCancelButton: PropTypes.bool,
+  onChange: PropTypes.func,
+  onChangeText: PropTypes.func,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
+  onSearchButtonPress: PropTypes.func,
+  onCancelButtonPress: PropTypes.func,
+  enablesReturnKeyAutomatically: PropTypes.bool,
+  hideBackground: PropTypes.bool,
+  barStyle: PropTypes.oneOf(['default', 'black']),
+  searchBarStyle: PropTypes.oneOf(['default', 'prominent', 'minimal']),
+  editable: PropTypes.bool
+};
+
+SearchBar.defaultProps = {
+  barStyle: 'default',
+  searchBarStyle: 'default',
+  editable: true
+};
 
 module.exports = SearchBar;
